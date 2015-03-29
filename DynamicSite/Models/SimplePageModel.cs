895b971882ad0fc8b0fc31fa0cf2e7fdccc2353c
Pat Howell <patrick.howell@outlook.com>
2015-03-29 21:14:17 +0100

    Added basic page model

---
+++ DynamicSite/Models/SimplePageModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DynamicSite.Models
+{
+    /// <summary>
+    /// Model for simple pages in the site
+    /// </summary>
+    public class SimplePageModel
+    {
+        /// <summary>
+        /// Page title
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Leader section for the page. Will be used for lower numbers header tags
+        /// </summary>
+        public string Leader { get; set; }
+
+        /// <summary>
+        /// Collection of strings which populate parahraphs. Each entry becomes a different paragraph.
+        /// </summary>
+        public IList<string> Paragraphs { get; set; }
+    }
+}
