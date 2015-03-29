5661a2e8de836e56abd32c77127ddf6084c33974
Pat Howell <patrick.howell@outlook.com>
2015-03-29 22:19:47 +0100

    Added view for the simple page model that I had added. Wired up to existing demo site to check it works.

--- DynamicSite/Controllers/HomeController.cs
+++ DynamicSite/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DynamicSite.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -25,6 +26,23 @@ namespace DynamicSite.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult SimplePage()
+        {
+            var model = GetBasicTestModel();
+
+            return View(model);
+        }
+
+        private SimplePageModel GetBasicTestModel()
+        {
+            return new SimplePageModel
+            {
+                Title = "Test title",
+                Leader = "Lead paragraph",
+                Paragraphs = new List<string> { "para 1", "para 2" }
+            };
         }
     }
 }
