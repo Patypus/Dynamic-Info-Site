1cd77c2b9a094752e3cdb4159366862b5beceb4d
Pat Howell <patrick.howell@outlook.com>
2015-04-09 22:01:49 +0100

    Added interface to the server and setup empty implementation. Added stub test class in test project for it.

---
+++ DynamicSiteServer/Facade/IServer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicSiteServer.Facade
+{
+    /// <summary>
+    /// Interface defining a server facade class which can act as a single point of contact for the web components
+    /// to get data from the server for display.
+    /// </summary>
+    public interface IServer
+    {
+    }
+}
