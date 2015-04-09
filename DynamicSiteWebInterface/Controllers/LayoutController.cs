71bc4111afd8070f2e495f00436af2dabd64c286
Pat Howell <patrick.howell@outlook.com>
2015-04-09 21:42:56 +0100

    Added new project for the server project

---
+++ DynamicSiteWebInterface/Controllers/LayoutController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DynamicSiteWebInterface.Controllers
+{
+    public class LayoutController : Controller
+    {
+        public ActionResult MainLayout()
+        {
+            return View();
+        }
+    }
+}
