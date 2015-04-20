38274aaeda321eb0a6924dc1e5a23c6fb5510e42
Pat Howell <patrick.howell@outlook.com>
2015-04-20 22:29:47 +0100

    Started adding config object and loader for site meta config

---
+++ DynamicSiteServer/Loader/ConfigLoader.cs
@@ -0,0 +1,18 @@
+﻿using DynamicSiteServer.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicSiteServer.Loader
+{
+    internal class ConfigLoader
+    {
+        internal SiteConfig LoadConfig(Path configPath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DynamicSiteServer/Model/SiteConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicSiteServer.Model
+{
+    /// <summary>
+    /// Object representing the site meta config for reading in
+    /// </summary>
+    public class SiteConfig
+    {
+        public string SiteName;
+
+        public string Author;
+    }
+}
